a7788975cdeb7f9d5d19ad4f30a7d59206f75dc8
Farjana Reetu <farjana.reetu@gmail.com>
2018-07-28 18:56:18 -0400

    fixed the schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,17 +1,3 @@
 DROP DATABASE IF EXISTS pokedex;
 
 CREATE DATABASE pokedex;
-
-USE pokedex;
-
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(85) NOT NULL,
-    favorite_pokemon1 VARCHAR(90),
-    favorite_pokemon2 VARCHAR(90),
-    favorite_pokemon3 VARCHAR(90),
-    favorite_pokemon4 VARCHAR(90),
-    favorite_pokemon5 VARCHAR(90),
-    favorite_pokemon6 VARCHAR(90),
-    PRIMARY KEY(id)
-);
